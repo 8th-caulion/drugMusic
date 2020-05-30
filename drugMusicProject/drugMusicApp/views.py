5c87217d6d39af2ea3bfa1ed95ba2fd8b127d5db
JongwonYoun <jeffyoun@naver.com>
2020-05-30 14:21:18 +0900

    profile 기본화면 완성

--- drugMusicProject/drugMusicApp/views.py
+++ drugMusicProject/drugMusicApp/views.py
@@ -2,19 +2,27 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .schedule import getSchedule
 from .models import Profile, Video
 
+def categoryString(index):
+    categories = ['발라드', 'R&B/소울', '인디락', '랩/힙합', '포크/어쿠스틱', '신스팝', '재즈']
+    return categories[index]
+
+def categoryChangedProfile(profile):
+    profile.category = categoryString(profile.category)
+    return profile
+
 # Create your views here.
 def main(request):
     profiles = Profile.objects.all
     return render(request, 'main.html', {"profiles":profiles})
 
 def profile(request):
-    profiles = Profile.objects.all
+    profiles = Profile.objects.all()
+    profiles = list(map(categoryChangedProfile, profiles))
     return render(request, 'profile.html', {"profiles":profiles})
 
 def detailProfile(request, id):
     profile = get_object_or_404(Profile, pk=id)
-    categories = ['발라드', 'R&B/소울', '인디락', '랩/힙합', '포크/어쿠스틱', '신스팝', '재즈']
-    profile.category = categories[profile.category]
+    profile.category = categoryString(profile.category)
     return render(request, 'profileDetail.html', {'profile' : profile})
 
 def profileNew(request):
